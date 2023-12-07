bc425ca6779b2681afe70769ece0fb910f592d8a
b4shtirk1n <wowan4512@gmail.com>
2023-12-08 00:41:54 +0500

    tests

--- STREAMUSEAPI/Controllers/UserController.cs
+++ STREAMUSEAPI/Controllers/UserController.cs
@@ -61,6 +61,7 @@ namespace STREAMUSEAPI.Controllers
             });
             await context.SaveChangesAsync();
             Log.Information("User added");
+
             return Ok();
         }
 
